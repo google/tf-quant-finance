6830f29022746838677ecca420aeff190943c5ed
Matej Rizman <matejr@google.com>
2019-08-15 11:17:05 +0100

    Remove remnants of internal project naming in one docstring.
    PiperOrigin-RevId: 263530441

--- random/__init__.py
+++ random/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Nomisma Quantitative Finance random number samplers."""
+"""Random number samplers."""
 
 from __future__ import absolute_import
 from __future__ import division
