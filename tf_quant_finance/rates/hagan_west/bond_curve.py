24237bea3fd9b67d60212204032e8cbda6e97f1c
Matej Rizman <matejr@google.com>
2019-09-11 05:19:00 -0700

    Remove unnecessary "from __future__ import".
    PiperOrigin-RevId: 268437115

--- tf_quant_finance/rates/hagan_west/bond_curve.py
+++ tf_quant_finance/rates/hagan_west/bond_curve.py
@@ -40,7 +40,6 @@ method described by Hagan and West in Ref [1, 2].
 
 from __future__ import absolute_import
 from __future__ import division
-from __future__ import google_type_annotations
 from __future__ import print_function
 
 import collections
