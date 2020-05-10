6130a23056cd18c5acd9ca81be510a1e2a827389
Fabrice DESCHATRES <27808402+fabrice-deschatres@users.noreply.github.com>
2020-05-10 13:30:51 +0100

    add heston analytics (#30)

--- tf_quant_finance/__init__.py
+++ tf_quant_finance/__init__.py
@@ -75,6 +75,7 @@ from tf_quant_finance import experimental
 from tf_quant_finance import math
 from tf_quant_finance import models
 from tf_quant_finance import rates
+from tf_quant_finance import volatility
 from tensorflow.python.util.all_util import remove_undocumented  # pylint: disable=g-direct-tensorflow-import
 
 _allowed_symbols = [
@@ -83,6 +84,7 @@ _allowed_symbols = [
     "experimental",
     "math",
     "models",
+    "volatility",
     "rates",
 ]
 
