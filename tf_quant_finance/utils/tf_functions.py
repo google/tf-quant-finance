ed9c96035ba46b9d7596663c1cd569e3c2d1f928
Ashish Saxena <saxeas@google.com>
2020-12-08 12:49:15 -0800

    Add a utility to help with converting nested inputs into a flat form for tf.functions.
    PiperOrigin-RevId: 346389187

---
+++ tf_quant_finance/utils/tf_functions.py
@@ -0,0 +1,78 @@
+# Lint as: python3
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Functions to deal with tf.function objects."""
+
+from typing import Any, Dict, Tuple, Union, Iterator, List, Optional
+import dataclasses
+
+# Dataclasses do not have a specific base type but do have a common interface
+# to convert them to dictionaries. We treat them as non-opaque objects whose
+# fields will be traversed.
+DataClassType = Any
+# Anything which is not a dataclass or a dictionary is treated as a leaf node
+# and terminates the traversal.
+LeafType = Any
+
+# A nested dictionary with string keys and dictionary or dataclass values which
+# will also be traversed.
+NestedDict = Dict[str, Union[Any, DataClassType, 'NestedDict']]
+
+# Ordered collections of keys starting from the outermost key to the innermost.
+KeyList = List[str]
+
+
+def iterate_nested(
+    nd: NestedDict,
+    previous_keys: Optional[KeyList] = None
+) -> Iterator[Tuple[KeyList, LeafType]]:
+  """Creates an iterator over every leaf value in depth first order.
+
+  Iterates over a nested dictionary in depth first order. The order in which
+  the peer keys are traversed is not guaranteed (same as for the keys of a
+  dictionary).
+
+  ```Example
+  nested_dict = {'a': 1, 'b': [2, 3, 4], 'c': {'d': 8}}
+  for k, v in iterate_nested(nested_dict):
+    print('_'.join(k), v)
+  # Prints out:
+  # a: 1
+  # b: [2, 3, 4]
+  # c_d: 8
+  ```
+
+  Args:
+    nd: The dictionary to be traversed.
+    previous_keys: If supplied, the computed key list will be a join of the
+      previous_keys and the current keys.
+
+  Yields:
+    A tuple of the key path and the value for each leaf node.
+  """
+  if previous_keys is None:
+    previous_keys = []
+  for k, v in nd.items():
+    keys = previous_keys + [k]
+    if not _is_nested(v):
+      yield keys, v
+    else:  # It is nested.
+      as_dict = dataclasses.asdict(v) if dataclasses.is_dataclass(v) else v
+      for val in iterate_nested(as_dict, keys):
+        yield val
+
+
+def _is_nested(x: Any) -> bool:
+  """Returns whether a value is nested."""
+  return isinstance(x, dict) or dataclasses.is_dataclass(x)
---
+++ tf_quant_finance/utils/tf_functions_test.py
@@ -0,0 +1,101 @@
+# Lint as: python3
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for tf_functions."""
+
+from typing import Dict
+import dataclasses
+import tensorflow.compat.v2 as tf
+
+import tf_quant_finance as tff
+from tensorflow.python.framework import test_util  # pylint: disable=g-direct-tensorflow-import
+
+
+@dataclasses.dataclass
+class TestDataclass:
+  simple_field: int
+  dict_field: Dict[str, int]
+
+
+class TfFunctionsTest(tf.test.TestCase):
+
+  def _assert_keys_values(self,
+                          iterator,
+                          expected_keys=None,
+                          expected_values=None):
+    key_lists, values = zip(*list(iterator))
+    keys = ['_'.join(k) for k in key_lists]
+    if expected_keys is not None and expected_values is not None:
+      result_dict = {k: v for k, v in zip(keys, values)}
+      expected_dict = {k: v for k, v in zip(expected_keys, expected_values)}
+      self.assertDictEqual(result_dict, expected_dict)
+      return
+
+    if expected_keys is not None:
+      self.assertSameElements(keys, expected_keys)
+      return
+
+    if expected_values is not None:
+      self.assertSameElements(values, expected_values)
+
+  def test_non_nested(self):
+    d = {'a': 1, 'b': 2}
+    iterator = tff.utils.iterate_nested(d)
+    self._assert_keys_values(
+        iterator, expected_keys=['a', 'b'], expected_values=[1, 2])
+
+  def test_empty(self):
+    items = []
+    for item in tff.utils.iterate_nested({}):
+      items.append(item)
+    self.assertEmpty(items)
+
+  def test_array_values(self):
+    d = {'a': [1, 2, 3], 'b': {'c': [4, 5]}}
+    self._assert_keys_values(
+        tff.utils.iterate_nested(d),
+        expected_keys=['a', 'b_c'],
+        expected_values=[[1, 2, 3], [4, 5]])
+
+  def test_nested(self):
+    nested_dict = {'a': 1, 'b': [2, 3, 4], 'c': {'d': 8}}
+    self._assert_keys_values(
+        tff.utils.iterate_nested(nested_dict),
+        expected_keys=['a', 'b', 'c_d'],
+        expected_values=[1, [2, 3, 4], 8])
+
+  def test_dataclass(self):
+    d = {
+        'a': {
+            'b': {
+                'c': 5.1
+            }
+        },
+        'data': TestDataclass(simple_field=42, dict_field={
+            'a': 6,
+            'z': 10
+        })
+    }
+    expected_keys = [
+        'a_b_c', 'data_simple_field', 'data_dict_field_a', 'data_dict_field_z'
+    ]
+    expected_vals = [5.1, 42, 6, 10]
+    self._assert_keys_values(
+        tff.utils.iterate_nested(d),
+        expected_keys=expected_keys,
+        expected_values=expected_vals)
+
+
+if __name__ == '__main__':
+  tf.test.main()
