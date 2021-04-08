Two minor adjustments to sabr approximations: 1. Fix nans that can occur when at-the-money 2. Add test demonstrating explicitly how array-broadcasting of the parameters work.
PiperOrigin-RevId: 367443868

tf_quant_finance/models/sabr/approximations/implied_volatility.py
@@ -149,8 +149,12 @@ def implied_volatility(*,
     strikes = tf.convert_to_tensor(strikes, dtype=dtype, name='strikes')
     dtype = dtype or strikes.dtype
 
-    forwards = tf.convert_to_tensor(forwards, dtype=dtype, name='forwards')
     expiries = tf.convert_to_tensor(expiries, dtype=dtype, name='expiries')
+    forwards = tf.convert_to_tensor(forwards, dtype=dtype, name='forwards')
+    alpha = tf.convert_to_tensor(alpha, dtype=dtype, name='alpha')
+    beta = tf.convert_to_tensor(beta, dtype=dtype, name='beta')
+    rho = tf.convert_to_tensor(rho, dtype=dtype, name='rho')
+    nu = tf.convert_to_tensor(nu, dtype=dtype, name='nu')
 
     moneyness = forwards / strikes
     log_moneyness = tf.math.log(moneyness)
@@ -160,17 +164,10 @@ def implied_volatility(*,
     # adjusted alpha = alpha * K^(beta - 1)
     adj_alpha = alpha * tf.math.pow(strikes, beta - 1.0)
 
-    def _zeta_by_xhat_if_true():
-      # Zeta, as defined in (eq. A.69b in [1])
-      zeta = ((nu / adj_alpha) * sqrt_adj_moneyness * log_moneyness)
-
-      # x-hat, as defined in (eq. A.69b in [1])
-      x_hat = _x_hat(zeta, rho)
-
-      return zeta / x_hat
-
-    # When nu == 0, the limit of zeta / x_hat(zeta) reduces to 1.0
-    zeta_by_xhat = tf.cond(nu > 0.0, _zeta_by_xhat_if_true, lambda: 1.0)
+    # Zeta, as defined in (eq. A.69b in [1])
+    zeta = (nu / adj_alpha) * sqrt_adj_moneyness * log_moneyness
+    # Zeta / xhat(zeta), as defined in (eq. A.69b in [1])
+    zeta_by_xhat = _zeta_by_xhat(zeta, rho)
 
     # This is the denominator term occurring in the ((1 + ...) / (1 + ...)) of
     # (eq. A.69a) in [1].
@@ -206,10 +203,14 @@ def implied_volatility(*,
       raise ValueError('Invalid value of `volatility_type`')
 
 
-def _x_hat(zeta, rho):
-  return tf.math.log(
-      (tf.math.sqrt(1 - 2 * rho * zeta + zeta * zeta) - rho + zeta) /
-      (1.0 - rho))
+def _zeta_by_xhat(zeta, rho):
+  zbxh = tf.math.divide_no_nan(
+      zeta,
+      tf.math.log(
+          (tf.math.sqrt(1 - 2 * rho * zeta + zeta * zeta) - rho + zeta) /
+          (1.0 - rho)))
+  # When zeta -> 0, the limit of zeta / x_hat(zeta) reduces to 1.0
+  return tf.where(zeta != 0.0, zbxh, 1.0)
 
 
 def _denom(beta, log_f_by_k):

tf_quant_finance/models/sabr/approximations/implied_volatility_test.py
@@ -55,9 +55,79 @@ class SabrApproximationImpliedVolatilityTest(parameterized.TestCase,
     # ```
     self.assertAllClose(equiv_vol, [0.33284656705268817, 1.9828728139982792])
 
+  def test_implied_volatility_parameter_broadcasting(self):
+    equiv_vol = tff.models.sabr.approximations.implied_volatility(
+        strikes=np.array([
+            [105.0, 110.0, 115.0, 120.0, 125.0],
+            [105.0, 110.0, 115.0, 120.0, 125.0],
+            [105.0, 110.0, 115.0, 120.0, 125.0],
+        ]),
+        expiries=np.array([
+            [30.0 / 365.0],
+            [60.0 / 365.0],
+            [90.0 / 365.0],
+        ]),
+        forwards=115.0,
+        alpha=np.array([
+            [1.00],
+            [1.50],
+            [2.00],
+        ]),
+        beta=np.array([
+            [0.25],
+            [0.50],
+            [0.75],
+        ]),
+        rho=0.05,
+        nu=np.array([
+            [2.0],
+            [2.5],
+            [3.0],
+        ]),
+        dtype=tf.float64)
+    equiv_vol = self.evaluate(equiv_vol)
+
+    self.assertAllClose(
+        equiv_vol,
+        [
+            [
+                # These correspond to:
+                # strikes=[105.0, 110.0, 115.0, 120.0, 125.0],
+                # For expiries=30.0/365.0, forwards=115.0, alpha=1.0, beta=0.25,
+                # rho=0.05, and nu=2.0.
+                0.07290126718857293,
+                0.048678694833488044,
+                0.02925354052258421,
+                0.048663433935318724,
+                0.06968554457600247,
+            ],
+            [
+                # These correspond to:
+                # strikes=[105.0, 110.0, 115.0, 120.0, 125.0],
+                # For expiries=60.0/365.0, forwards=115.0, alpha=1.5, beta=0.5,
+                # rho=0.05, and nu=2.5.
+                0.19479147227269086,
+                0.1647194927224411,
+                0.15186141282688834,
+                0.16548343518393138,
+                0.19098676373872667,
+            ],
+            [
+                # These correspond to:
+                # strikes=[105.0, 110.0, 115.0, 120.0, 125.0],
+                # For expiries=90.0/365.0, forwards=115.0, alpha=2.0, beta=0.75,
+                # rho=0.05, and nu=3.0.
+                0.7493941577599591,
+                0.7318045979333434,
+                0.7259931664225101,
+                0.7310817574988578,
+                0.7449168095317619,
+            ],
+        ])
+
   @parameterized.named_parameters(
       {
-          'testcase_name': 'generic_example_1',
+          'testcase_name': 'generic_example',
           'strikes': 130.0,
           'forwards': 110.0,
           'expiries': 200.0 / 365.0,
@@ -107,6 +177,16 @@ class SabrApproximationImpliedVolatilityTest(parameterized.TestCase,
           'rho': 0.2,
           'expected_vol': 0.1400903903272142
       }, {
+          'testcase_name': 'at_the_money',
+          'strikes': 130.0,
+          'forwards': 130.0,
+          'expiries': 0.5,
+          'alpha': 0.31,
+          'beta': 0.55,
+          'nu': 2.5,
+          'rho': 0.2,
+          'expected_vol': 0.043211700217443874
+      }, {
           'testcase_name': 'tensor_inputs',
           'strikes': np.array([11.0, 15.0]),
           'forwards': np.array([10.0, 11.0]),
@@ -138,7 +218,7 @@ class SabrApproximationImpliedVolatilityTest(parameterized.TestCase,
 
   @parameterized.named_parameters(
       {
-          'testcase_name': 'generic_example_1',
+          'testcase_name': 'generic_example',
           'strikes': 130.0,
           'forwards': 110.0,
           'expiries': 200.0 / 365.0,
@@ -214,6 +294,114 @@ class SabrApproximationImpliedVolatilityTest(parameterized.TestCase,
         volatility_type=NORMAL)
     equiv_vol = self.evaluate(equiv_vol)
     self.assertAllClose(expected_vol, equiv_vol)
+
+  @parameterized.product(
+      (
+          # Generic scalar example.
+          {
+              'strikes': 130.0,
+              'forwards': 110.0,
+              'expiries': 200.0 / 365.0,
+              'alpha': 0.31,
+              'beta': 0.5,
+              'nu': 2.8,
+              'rho': -0.02,
+          },
+          # Generic scalar pathological example: at-the-money.
+          {
+              'strikes': 130.0,
+              'forwards': 130.0,
+              'expiries': 200.0 / 365.0,
+              'alpha': 0.31,
+              'beta': 0.5,
+              'nu': 2.8,
+              'rho': -0.02,
+          },
+          # Generic scalar pathological example: beta = 0
+          {
+              'strikes': 130.0,
+              'forwards': 130.0,
+              'expiries': 200.0 / 365.0,
+              'alpha': 0.31,
+              'beta': 0.0,
+              'nu': 2.8,
+              'rho': -0.02,
+          },
+          # Generic scalar pathological example: beta = 1
+          {
+              'strikes': 130.0,
+              'forwards': 130.0,
+              'expiries': 200.0 / 365.0,
+              'alpha': 0.31,
+              'beta': 1.0,
+              'nu': 2.8,
+              'rho': -0.02,
+          },
+          # Generic scalar pathological example: nu = 0
+          {
+              'strikes': 130.0,
+              'forwards': 130.0,
+              'expiries': 200.0 / 365.0,
+              'alpha': 0.31,
+              'beta': 0.5,
+              'nu': 0.0,
+              'rho': -0.02,
+          },
+          # Generic scalar pathological example: expiries = 0.0
+          {
+              'strikes': 130.0,
+              'forwards': 130.0,
+              'expiries': 0.0 / 365.0,
+              'alpha': 0.31,
+              'beta': 0.5,
+              'nu': 2.8,
+              'rho': -0.02,
+          },
+          # Generic example, with tensor values, including at-the-money case and
+          # expiry = 0.0 case.
+          {
+              'strikes':
+                  np.array([[130.0, 140.0, 150.0], [130.0, 140.0, 150.0]]),
+              'forwards':
+                  140.0,
+              'expiries': [[0.0], [1.0]],
+              'alpha': [[0.25], [0.5]],
+              'beta': [[0.33], [0.66]],
+              'nu': [[1.0], [2.0]],
+              'rho': [[0.001], [-0.001]],
+          }),
+      vol_type=(NORMAL, LOGNORMAL),
+  )
+  def test_implied_volatility_differentiable(self, strikes, forwards, expiries,
+                                             alpha, beta, nu, rho, vol_type):
+    dtype = tf.float64
+
+    forwards = tf.convert_to_tensor(forwards, dtype=dtype)
+    strikes = tf.convert_to_tensor(strikes, dtype=dtype)
+    expiries = tf.convert_to_tensor(expiries, dtype=dtype)
+    alpha = tf.convert_to_tensor(alpha, dtype=dtype)
+    beta = tf.convert_to_tensor(beta, dtype=dtype)
+    rho = tf.convert_to_tensor(rho, dtype=dtype)
+    nu = tf.convert_to_tensor(nu, dtype=dtype)
+
+    with tf.GradientTape(persistent=True) as tape:
+      tape.watch([forwards, strikes, expiries, alpha, beta, rho, nu])
+      equiv_vol = tff.models.sabr.approximations.implied_volatility(
+          forwards=forwards,
+          strikes=strikes,
+          expiries=expiries,
+          alpha=alpha,
+          beta=beta,
+          rho=rho,
+          nu=nu,
+          volatility_type=vol_type,
+          dtype=dtype)
+      grad = tape.gradient(
+          target=equiv_vol,
+          sources=[forwards, strikes, expiries, alpha, beta, rho, nu])
+
+    grad = self.evaluate(grad)
+    self.assertTrue(all(np.all(np.isfinite(x)) for x in grad))
 
 
 if __name__ == '__main__':