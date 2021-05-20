d99c50258cfcd12346f439d3bef1e733867cc707
Cyril Chimisov <cyrilchimisov@google.com>
2021-05-20 07:18:05 -0700

    Enable graph mode tests for the Hull-White model.
    PiperOrigin-RevId: 374865031

--- tf_quant_finance/models/hull_white/hull_white_test.py
+++ tf_quant_finance/models/hull_white/hull_white_test.py
@@ -25,7 +25,7 @@ import tf_quant_finance as tff
 from tensorflow.python.framework import test_util  # pylint: disable=g-direct-tensorflow-import
 
 
-# @test_util.run_all_in_graph_and_eager_modes
+@test_util.run_all_in_graph_and_eager_modes
 class HullWhiteTest(parameterized.TestCase, tf.test.TestCase):
 
   def setUp(self):
