f06719dc3b5acb526f0d5fe7f13fdb9cf3c1d714
Cyril Chimisov <cyrilchimisov@google.com>
2022-01-07 09:39:02 -0800

    Update pip package
    PiperOrigin-RevId: 420310658

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ else:
   # The suffix should be replaced with 'aN', 'bN', or 'rcN' (note: no dots) for
   # respective alpha releases, beta releases, and release candidates. And it
   # should be cleared, i.e. set to '', for stable releases (c.f. PEP 440).
-  release_suffix = '.dev29'
+  release_suffix = '.dev30'
   tfp_package = 'tensorflow-probability >= 0.11.0'
 
 __version__ = '.'.join([major_version, minor_version, patch_version])
