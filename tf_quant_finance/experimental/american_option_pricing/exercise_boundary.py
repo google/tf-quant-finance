2b186a303694c82452e1c40944aa8ea09b17bb06
Viktoria Csizmadia <viktoriac@google.com>
2022-10-20 05:48:38 -0700

    Calculating the denominator for the exercise boundary function for American options.
    PiperOrigin-RevId: 482465668

--- tf_quant_finance/experimental/american_option_pricing/exercise_boundary.py
+++ tf_quant_finance/experimental/american_option_pricing/exercise_boundary.py
@@ -41,8 +41,8 @@ def boundary_numerator(
   """Subcalculation to get the exercise boundary function of an American option.
 
   Evaluates the numerator part of the calculation required to get the exercise
-  boundary function of an American option. This corresponds to `N` in the paper
-  [1].
+  boundary function of an American option. This corresponds to `N` in formula
+  (3.7) in the paper [1].
 
   #### References
   [1] Leif Andersen, Mark Lake and Dimitri Offengenden. High-performance
@@ -105,6 +105,91 @@ def boundary_numerator(
       return tf.math.exp(r_exp * u) * norm
 
     term2 = r * gauss_legendre(
+        func=func,
+        lower=tf.zeros_like(tau),
+        upper=tau,
+        num_points=integration_num_points,
+        dtype=dtype)
+    return term1 + term2
+
+
+def boundary_denominator(
+    tau: types.FloatTensor,
+    b: Callable[[types.FloatTensor, types.FloatTensor, types.FloatTensor,
+                 types.FloatTensor], types.FloatTensor],
+    k: types.FloatTensor,
+    r: types.FloatTensor,
+    q: types.FloatTensor,
+    sigma: types.FloatTensor,
+    integration_num_points: int = 32,
+    dtype: Optional[tf.DType] = None) -> types.FloatTensor:
+  """Subcalculation to get the exercise boundary function of an American option.
+
+  Evaluates the denominator part of the calculation required to get the exercise
+  boundary function of an American option. This corresponds to `D` in formula
+  (3.8) in the paper [1].
+
+  #### References
+  [1] Leif Andersen, Mark Lake and Dimitri Offengenden. High-performance
+  American option pricing. 2015
+  https://engineering.nyu.edu/sites/default/files/2019-03/Carr-adjusting-exponential-levy-models.pdf#page=54
+
+  #### Example
+  ```python
+    def b_0(tau, k, r, q):
+      return k * tf.math.minimum(1, r/q)
+    tau = tf.constant([1, 2], dtype=tf.float64)
+    k = tf.constant([100, 100], dtype=tf.float64)
+    r = tf.constant([0.01, 0.02], dtype=tf.float64)
+    q = tf.constant([0.01, 0.02], dtype=tf.float64)
+    sigma = tf.constant([0.1, 0.15], dtype=tf.float64)
+    integration_num_points = 32
+    boundary_denominator(tau, b_0, k, r, q, sigma, integration_num_points)
+    # tf.constant([0.52509737, 0.56378576], dtype=tf.float64)
+  ```
+
+  Args:
+    tau: Grid of values of shape `batch_shape` indicating the time left until
+      option maturity.
+    b: Represents the exercise boundary function for the option.
+    k: Same shape and dtype as `tau` representing the strike price of the
+      option.
+    r: Same shape and dtype as `tau` representing the annualized risk-free
+      interest rate, continuously compounded.
+    q: Same shape and dtype as `tau` representing the dividend rate.
+    sigma: Same shape and dtype as `tau` representing the volatility of the
+      option's returns.
+    integration_num_points: The number of points used in the integration
+      approximation method.
+      Default value: 32.
+    dtype: If supplied, the dtype for all input tensors. Result will have the
+      same dtype.
+      Default value: None which maps to dtype of `tau`.
+
+  Returns:
+    `Tensor` of shape `batch_shape`, containing a partial result for calculating
+    the exercise boundary.
+  """
+  with tf.name_scope('calculate_D'):
+    tau = tf.convert_to_tensor(tau, dtype=dtype)
+    dtype = tau.dtype
+    k = tf.convert_to_tensor(k, dtype=dtype)
+    r = tf.convert_to_tensor(r, dtype=dtype)
+    q = tf.convert_to_tensor(q, dtype=dtype)
+    sigma = tf.convert_to_tensor(sigma, dtype=dtype)
+    term1 = _standard_normal_cdf(_d_plus(tau, b(tau, k, r, q)/k, r, q, sigma))
+    def func(u):
+      tau_exp = tf.expand_dims(tau, axis=-1)
+      k_exp = tf.expand_dims(k, axis=-1)
+      r_exp = tf.expand_dims(r, axis=-1)
+      q_exp = tf.expand_dims(q, axis=-1)
+      sigma_exp = tf.expand_dims(sigma, axis=-1)
+      ratio = b(tau_exp, k_exp, r_exp, q_exp) / b(u, k_exp, r_exp, q_exp)
+      norm = _standard_normal_cdf(
+          _d_plus(tau_exp - u, ratio, r_exp, q_exp, sigma_exp))
+      return tf.math.exp(q_exp * u) * norm
+
+    term2 = q * gauss_legendre(
         func=func,
         lower=tf.zeros_like(tau),
         upper=tau,
--- tf_quant_finance/experimental/american_option_pricing/exercise_boundary_test.py
+++ tf_quant_finance/experimental/american_option_pricing/exercise_boundary_test.py
@@ -7,6 +7,7 @@ from tensorflow.python.framework import test_util  # pylint: disable=g-direct-te
 
 
 boundary_numerator = tff.experimental.american_option_pricing.exercise_boundary.boundary_numerator
+boundary_denominator = tff.experimental.american_option_pricing.exercise_boundary.boundary_denominator
 
 
 class ExerciseBoundaryTest(parameterized.TestCase, tf.test.TestCase):
@@ -19,7 +20,7 @@ class ExerciseBoundaryTest(parameterized.TestCase, tf.test.TestCase):
           'r': [],
           'q': [],
           'sigma': [],
-          'expected_boundary': []
+          'expected_numerator': []
       },
       {
           'testcase_name': 'TestSingle',
@@ -28,7 +29,7 @@ class ExerciseBoundaryTest(parameterized.TestCase, tf.test.TestCase):
           'r': 0.15,
           'q': 0.05,
           'sigma': 0.02,
-          'expected_boundary': 1.56363801,
+          'expected_numerator': 1.56363801,
       },
       {
           'testcase_name': 'TestMultiple',
@@ -37,23 +38,62 @@ class ExerciseBoundaryTest(parameterized.TestCase, tf.test.TestCase):
           'r': [0.01, 0.02],
           'q': [0.01, 0.02],
           'sigma': [0.1, 0.15],
-          'expected_boundary': [0.4849528, 0.47702501],
+          'expected_numerator': [0.4849528, 0.47702501],
       },
   )
-  def test_numerator(self, k, tau, r, q, sigma, expected_boundary):
+  def test_numerator(self, k, tau, r, q, sigma, expected_numerator):
     k = tf.constant(k, dtype=tf.float64)
     tau = tf.constant(tau, dtype=tf.float64)
     r = tf.constant(r, dtype=tf.float64)
     q = tf.constant(q, dtype=tf.float64)
     sigma = tf.constant(sigma, dtype=tf.float64)
-    expected_boundary = tf.constant(expected_boundary, dtype=tf.float64)
-    max_rel_error = 1e-8
+    expected_numerator = tf.constant(expected_numerator, dtype=tf.float64)
     def _b_0(_, k, r, q):
       return k * tf.math.minimum(1, r/q)
-    actual_boundary = boundary_numerator(tau, _b_0, k, r, q, sigma)
-    assert np.all(
-        np.abs(actual_boundary -
-               expected_boundary) <= np.abs(expected_boundary) * max_rel_error)
+    actual_numerator = boundary_numerator(tau, _b_0, k, r, q, sigma)
+    np.testing.assert_allclose(actual_numerator, expected_numerator, rtol=1e-8)
+
+  @parameterized.named_parameters(
+      {
+          'testcase_name': 'TestEmpty',
+          'k': [],
+          'tau': [],
+          'r': [],
+          'q': [],
+          'sigma': [],
+          'expected_denominator': []
+      },
+      {
+          'testcase_name': 'TestSingle',
+          'k': 45,
+          'tau': 3,
+          'r': 0.15,
+          'q': 0.05,
+          'sigma': 0.02,
+          'expected_denominator': 1.1606823164169706,
+      },
+      {
+          'testcase_name': 'TestMultiple',
+          'k': [100, 100],
+          'tau': [1, 2],
+          'r': [0.01, 0.02],
+          'q': [0.01, 0.02],
+          'sigma': [0.1, 0.15],
+          'expected_denominator': [0.52509737, 0.56378576],
+      },
+  )
+  def test_denominator(self, k, tau, r, q, sigma, expected_denominator):
+    k = tf.constant(k, dtype=tf.float64)
+    tau = tf.constant(tau, dtype=tf.float64)
+    r = tf.constant(r, dtype=tf.float64)
+    q = tf.constant(q, dtype=tf.float64)
+    sigma = tf.constant(sigma, dtype=tf.float64)
+    expected_denominator = tf.constant(expected_denominator, dtype=tf.float64)
+    def _b_0(_, k, r, q):
+      return k * tf.math.minimum(1, r/q)
+    actual_denominator = boundary_denominator(tau, _b_0, k, r, q, sigma)
+    np.testing.assert_allclose(
+        actual_denominator, expected_denominator, rtol=1e-8)
 
 
 if __name__ == '__main__':
