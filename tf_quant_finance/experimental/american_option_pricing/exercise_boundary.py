3039dd8ccbd002e0f638f16e36d7f17f0900cbe0
Viktoria Csizmadia <viktoriac@google.com>
2022-11-03 11:36:42 -0700

    Implement exercise_boundary function for American option pricing algorithm.
    PiperOrigin-RevId: 485929179

--- tf_quant_finance/experimental/american_option_pricing/exercise_boundary.py
+++ tf_quant_finance/experimental/american_option_pricing/exercise_boundary.py
@@ -4,9 +4,13 @@ import numpy as np
 import tensorflow.compat.v2 as tf
 
 from tf_quant_finance import types
+from tf_quant_finance import utils
 from tf_quant_finance.math.integration import gauss_legendre
+from tf_quant_finance.math.interpolation.cubic import cubic_interpolation
 
 
+build_spline = cubic_interpolation.build
+interpolate = cubic_interpolation.interpolate
 _SQRT_2 = np.sqrt(2.0, dtype=np.float64)
 
 
@@ -29,18 +33,17 @@ def _d_minus(tau: types.FloatTensor, z: types.FloatTensor, r: types.FloatTensor,
 
 
 def boundary_numerator(
-    tau: types.FloatTensor,
-    b: Callable[[types.FloatTensor, types.FloatTensor, types.FloatTensor,
-                 types.FloatTensor], types.FloatTensor],
+    tau_grid: types.FloatTensor,
+    b: Callable[[types.FloatTensor], types.FloatTensor],
     k: types.FloatTensor,
     r: types.FloatTensor,
     q: types.FloatTensor,
     sigma: types.FloatTensor,
     integration_num_points: int = 32,
     dtype: Optional[tf.DType] = None) -> types.FloatTensor:
-  """Subcalculation to get the exercise boundary function of an American option.
+  """Calculates the numerator of the exercise boundary function of an American option.
 
-  Evaluates the numerator part of the calculation required to get the exercise
+  Calculates the numerator part of the calculation required to get the exercise
   boundary function of an American option. This corresponds to `N` in formula
   (3.7) in the paper [1].
 
@@ -51,83 +54,99 @@ def boundary_numerator(
 
   #### Example
   ```python
-    def b_0(tau, k, r, q):
-      return k * tf.math.minimum(1, r/q)
-    tau = tf.constant([1, 2], dtype=tf.float64)
+    tau_grid = tf.constant([[0., 0.5, 1.], [0., 1., 2.]], dtype=tf.float64)
     k = tf.constant([100, 100], dtype=tf.float64)
     r = tf.constant([0.01, 0.02], dtype=tf.float64)
     q = tf.constant([0.01, 0.02], dtype=tf.float64)
     sigma = tf.constant([0.1, 0.15], dtype=tf.float64)
+    k_exp = k[:, tf.newaxis, tf.newaxis]
+    r_exp = r[:, tf.newaxis, tf.newaxis]
+    q_exp = q[:, tf.newaxis, tf.newaxis]
+    def b_0(tau_grid_exp):
+      return tf.ones_like(tau_grid_exp) * k_exp * tf.math.minimum(
+          tf.constant(1.0, dtype=tf.float64), r_exp/q_exp)
     integration_num_points = 32
-    boundary_numerator(tau, b_0, k, r, q, sigma, integration_num_points)
-    # tf.constant([0.4849528 , 0.47702501], dtype=tf.float64)
+    boundary_numerator(tau_grid, b_0, k, r, q, sigma, integration_num_points)
+    # tf.constant([[0.5, 0.48835735, 0.4849528],
+    #             [0.5, 0.4798061, 0.47702501]], dtype=tf.float64)
   ```
 
   Args:
-    tau: Grid of values of shape `batch_shape` indicating the time left until
-      option maturity.
-    b: Represents the exercise boundary function for the option.
-    k: Same shape and dtype as `tau` representing the strike price of the
-      option.
-    r: Same shape and dtype as `tau` representing the annualized risk-free
+    tau_grid: Grid of values of shape `[num_options, grid_num_points]`
+      indicating the time left until option maturity.
+    b: Represents the exercise boundary function for the option. Receives
+      `Tensor` of rank `tau_grid.rank + 1` and returns a `Tensor` of same shape.
+    k: Same dtype as `tau_grid` with shape `num_options` representing the strike
+      price of the option.
+    r: Same shape and dtype as `k` representing the annualized risk-free
       interest rate, continuously compounded.
-    q: Same shape and dtype as `tau` representing the dividend rate.
-    sigma: Same shape and dtype as `tau` representing the volatility of the
+    q: Same shape and dtype as `k` representing the dividend rate.
+    sigma: Same shape and dtype as `k` representing the volatility of the
       option's returns.
     integration_num_points: The number of points used in the integration
       approximation method.
       Default value: 32.
     dtype: If supplied, the dtype for all input tensors. Result will have the
       same dtype.
-      Default value: None which maps to dtype of `tau`.
+      Default value: None which maps to dtype of `tau_grid`.
 
   Returns:
-    `Tensor` of shape `batch_shape`, containing a partial result for calculating
-    the exercise boundary.
+    `Tensor` of shape `[num_options, grid_num_points]`, containing a partial
+    result for calculating the exercise boundary.
   """
   with tf.name_scope('calculate_N'):
-    tau = tf.convert_to_tensor(tau, dtype=dtype)
-    dtype = tau.dtype
+    # Shape [num_options, grid_num_points]
+    tau_grid = tf.convert_to_tensor(tau_grid, dtype=dtype)
+    dtype = tau_grid.dtype
+    # Shape [num_options]
     k = tf.convert_to_tensor(k, dtype=dtype)
     r = tf.convert_to_tensor(r, dtype=dtype)
     q = tf.convert_to_tensor(q, dtype=dtype)
     sigma = tf.convert_to_tensor(sigma, dtype=dtype)
-    term1 = _standard_normal_cdf(_d_minus(tau, b(tau, k, r, q)/k, r, q, sigma))
+    # Shape [num_options, 1, 1]
+    k_exp = k[:, tf.newaxis, tf.newaxis]
+    r_simple_exp = r[:, tf.newaxis]
+    r_exp = r_simple_exp[:, tf.newaxis]
+    q_exp = q[:, tf.newaxis, tf.newaxis]
+    sigma_exp = sigma[:, tf.newaxis, tf.newaxis]
+    # Shape [num_options, grid_num_points, 1]
+    tau_grid_exp = tf.expand_dims(tau_grid, axis=-1)
+    term1 = _standard_normal_cdf(
+        _d_minus(tau_grid_exp,
+                 b(tau_grid_exp) / k_exp, r_exp, q_exp, sigma_exp))
+    # Shape [num_options, grid_num_points]
+    term1 = tf.squeeze(term1, axis=-1)
     def func(u):
-      tau_exp = tf.expand_dims(tau, axis=-1)
-      k_exp = tf.expand_dims(k, axis=-1)
-      r_exp = tf.expand_dims(r, axis=-1)
-      q_exp = tf.expand_dims(q, axis=-1)
-      sigma_exp = tf.expand_dims(sigma, axis=-1)
-      ratio = b(tau_exp, k_exp, r_exp, q_exp) / b(u, k_exp, r_exp, q_exp)
+      # Shape [num_options, grid_num_points, integration_num_points]
+      ratio = b(tau_grid_exp) / b(u)
       norm = _standard_normal_cdf(
-          _d_minus(tau_exp - u, ratio, r_exp, q_exp, sigma_exp))
+          _d_minus(tau_grid_exp - u, ratio, r_exp, q_exp, sigma_exp))
       return tf.math.exp(r_exp * u) * norm
 
-    term2 = r * gauss_legendre(
+    # Shape [num_options, grid_num_points]
+    term2 = r_simple_exp * gauss_legendre(
         func=func,
-        lower=tf.zeros_like(tau),
-        upper=tau,
+        lower=tf.zeros_like(tau_grid),
+        upper=tau_grid,
         num_points=integration_num_points,
         dtype=dtype)
     return term1 + term2
 
 
 def boundary_denominator(
-    tau: types.FloatTensor,
-    b: Callable[[types.FloatTensor, types.FloatTensor, types.FloatTensor,
-                 types.FloatTensor], types.FloatTensor],
+    tau_grid: types.FloatTensor,
+    b: Callable[[types.FloatTensor], types.FloatTensor],
     k: types.FloatTensor,
     r: types.FloatTensor,
     q: types.FloatTensor,
     sigma: types.FloatTensor,
     integration_num_points: int = 32,
     dtype: Optional[tf.DType] = None) -> types.FloatTensor:
-  """Subcalculation to get the exercise boundary function of an American option.
+  """Calculates the denominator of the exercise boundary function of an American option.
 
-  Evaluates the denominator part of the calculation required to get the exercise
-  boundary function of an American option. This corresponds to `D` in formula
-  (3.8) in the paper [1].
+  Calculates the denominator part of the calculation required to get the
+  exercise boundary function of an American option. This corresponds to `D` in
+  formula (3.8) in the paper [1].
 
   #### References
   [1] Leif Andersen, Mark Lake and Dimitri Offengenden. High-performance
@@ -136,29 +155,139 @@ def boundary_denominator(
 
   #### Example
   ```python
-    def b_0(tau, k, r, q):
-      return k * tf.math.minimum(1, r/q)
+    tau_grid = tf.constant([[0., 0.5, 1.], [0., 1., 2.]], dtype=tf.float64)
+    k = tf.constant([100, 100], dtype=tf.float64)
+    r = tf.constant([0.01, 0.02], dtype=tf.float64)
+    q = tf.constant([0.01, 0.02], dtype=tf.float64)
+    sigma = tf.constant([0.1, 0.15], dtype=tf.float64)
+    k_exp = k[:, tf.newaxis, tf.newaxis]
+    r_exp = r[:, tf.newaxis, tf.newaxis]
+    q_exp = q[:, tf.newaxis, tf.newaxis]
+    def b_0(tau_grid_exp):
+      return tf.ones_like(tau_grid_exp) * k_exp * tf.math.minimum(
+          tf.constant(1.0, dtype=tf.float64), r_exp/q_exp)
+    integration_num_points = 32
+    boundary_denominator(tau_grid, b_0, k, r, q, sigma, integration_num_points)
+    # tf.constant([[0.5, 0.51665517, 0.52509737],
+    #             [0.5, 0.54039524, 0.56378576]], dtype=tf.float64)
+  ```
+
+  Args:
+    tau_grid: Grid of values of shape `[num_options, grid_num_points]`
+      indicating the time left until option maturity.
+    b: Represents the exercise boundary function for the option. Receives
+      `Tensor` of rank `tau_grid.rank + 1` and returns a `Tensor` of same shape.
+    k: Same dtype as `tau_grid` with shape `num_options` representing the strike
+      price of the option.
+    r: Same shape and dtype as `k` representing the annualized risk-free
+      interest rate, continuously compounded.
+    q: Same shape and dtype as `k` representing the dividend rate.
+    sigma: Same shape and dtype as `k` representing the volatility of the
+      option's returns.
+    integration_num_points: The number of points used in the integration
+      approximation method.
+      Default value: 32.
+    dtype: If supplied, the dtype for all input tensors. Result will have the
+      same dtype.
+      Default value: None which maps to dtype of `tau_grid`.
+
+  Returns:
+    `Tensor` of shape `[num_options, grid_num_points]`, containing a partial
+    result for calculating the exercise boundary.
+  """
+  with tf.name_scope('calculate_D'):
+    # Shape [num_options, grid_num_points]
+    tau_grid = tf.convert_to_tensor(tau_grid, dtype=dtype)
+    dtype = tau_grid.dtype
+    # Shape [num_options]
+    k = tf.convert_to_tensor(k, dtype=dtype)
+    r = tf.convert_to_tensor(r, dtype=dtype)
+    q = tf.convert_to_tensor(q, dtype=dtype)
+    sigma = tf.convert_to_tensor(sigma, dtype=dtype)
+    # Shape [num_options, 1, 1]
+    k_exp = k[:, tf.newaxis, tf.newaxis]
+    r_exp = r[:, tf.newaxis, tf.newaxis]
+    q_simple_exp = q[:, tf.newaxis]
+    q_exp = q_simple_exp[:, tf.newaxis]
+    sigma_exp = sigma[:, tf.newaxis, tf.newaxis]
+    # Shape [num_options, grid_num_points, 1]
+    tau_grid_exp = tf.expand_dims(tau_grid, axis=-1)
+    term1 = _standard_normal_cdf(
+        _d_plus(tau_grid_exp, b(tau_grid_exp) / k_exp, r_exp, q_exp, sigma_exp))
+    # Shape [num_options, grid_num_points]
+    term1 = tf.squeeze(term1, axis=-1)
+    def func(u):
+      # Shape [num_options, grid_num_points, integration_num_points]
+      ratio = b(tau_grid_exp) / b(u)
+      norm = _standard_normal_cdf(
+          _d_plus(tau_grid_exp - u, ratio, r_exp, q_exp, sigma_exp))
+      return tf.math.exp(q_exp * u) * norm
+
+    # Shape [num_options, grid_num_points]
+    term2 = q_simple_exp * gauss_legendre(
+        func=func,
+        lower=tf.zeros_like(tau_grid),
+        upper=tau_grid,
+        num_points=integration_num_points,
+        dtype=dtype)
+    return term1 + term2
+
+
+def exercise_boundary(
+    tau_grid: types.FloatTensor,
+    k: types.FloatTensor,
+    r: types.FloatTensor,
+    q: types.FloatTensor,
+    sigma: types.FloatTensor,
+    max_depth: int = 20,
+    tolerance: float = 1e-8,
+    integration_num_points: int = 32,
+    dtype: Optional[tf.DType] = None
+) -> Callable[[types.FloatTensor], types.FloatTensor]:
+  """Calculates the exercise boundary function of an American option.
+
+  Iteratively calculates the exercise boundary function of an American option.
+  This corresponds to `B` in formula (3.9) in the paper [1].
+
+  #### References
+  [1] Leif Andersen, Mark Lake and Dimitri Offengenden. High-performance
+  American option pricing. 2015
+  https://engineering.nyu.edu/sites/default/files/2019-03/Carr-adjusting-exponential-levy-models.pdf#page=55
+
+  #### Example
+  ```python
     tau = tf.constant([1, 2], dtype=tf.float64)
     k = tf.constant([100, 100], dtype=tf.float64)
     r = tf.constant([0.01, 0.02], dtype=tf.float64)
     q = tf.constant([0.01, 0.02], dtype=tf.float64)
     sigma = tf.constant([0.1, 0.15], dtype=tf.float64)
+    grid_num_points = 3
+    max_depth = 30
+    tolerance = 1e-8
     integration_num_points = 32
-    boundary_denominator(tau, b_0, k, r, q, sigma, integration_num_points)
-    # tf.constant([0.52509737, 0.56378576], dtype=tf.float64)
+    tau_grid = tf.linspace(tf.constant(0., dtype=tf.float64), tau,
+                          grid_num_points, axis=-1)
+    exercise_boundary(tau_grid, k, r, q, sigma, max_depth, tolerance,
+                      integration_num_points)
+    # tf.constant([[100., 82.51395531, 80.49806099],
+    #             [100., 71.00509961, 69.96481827]], dtype=tf.float64)
   ```
 
   Args:
-    tau: Grid of values of shape `batch_shape` indicating the time left until
-      option maturity.
-    b: Represents the exercise boundary function for the option.
-    k: Same shape and dtype as `tau` representing the strike price of the
-      option.
-    r: Same shape and dtype as `tau` representing the annualized risk-free
+    tau_grid: Grid of values of shape `[num_options, grid_num_points]`
+      indicating the time left until option maturity.
+    k: Same dtype as `tau_grid` with shape `num_options` representing the strike
+      price of the option.
+    r: Same shape and dtype as `k` representing the annualized risk-free
       interest rate, continuously compounded.
-    q: Same shape and dtype as `tau` representing the dividend rate.
-    sigma: Same shape and dtype as `tau` representing the volatility of the
+    q: Same shape and dtype as `k` representing the dividend rate.
+    sigma: Same shape and dtype as `k` representing the volatility of the
       option's returns.
+    max_depth: Maximum number of iterations for calculating the exercise
+      boundary if it doesn't converge earlier. Default value: 20.
+    tolerance: Represents the tolerance for the relative difference between the
+      old and new exercise boundary function values, at which to stop further
+      calculating a new exercise boundary function.
     integration_num_points: The number of points used in the integration
       approximation method.
       Default value: 32.
@@ -167,32 +296,76 @@ def boundary_denominator(
       Default value: None which maps to dtype of `tau`.
 
   Returns:
-    `Tensor` of shape `batch_shape`, containing a partial result for calculating
-    the exercise boundary.
+    `Callable` expecting `Tensor` of shape `[num_options, grid_num_points, n]`
+    as input (where `n` is arbitrary)  and returning `Tensor` of the same shape.
+    Represents the exercise boundary function of an American option pricing
+    algorithm.
   """
-  with tf.name_scope('calculate_D'):
-    tau = tf.convert_to_tensor(tau, dtype=dtype)
-    dtype = tau.dtype
+  with tf.name_scope('exercise_boundary'):
+    # Shape [num_options, grid_num_points]
+    tau_grid = tf.convert_to_tensor(tau_grid, dtype=dtype)
+    dtype = tau_grid.dtype
+    epsilon = 1e-6
+    if dtype == tf.float64:
+      epsilon = 1e-10
+    # Shape [num_options]
     k = tf.convert_to_tensor(k, dtype=dtype)
     r = tf.convert_to_tensor(r, dtype=dtype)
     q = tf.convert_to_tensor(q, dtype=dtype)
     sigma = tf.convert_to_tensor(sigma, dtype=dtype)
-    term1 = _standard_normal_cdf(_d_plus(tau, b(tau, k, r, q)/k, r, q, sigma))
-    def func(u):
-      tau_exp = tf.expand_dims(tau, axis=-1)
-      k_exp = tf.expand_dims(k, axis=-1)
-      r_exp = tf.expand_dims(r, axis=-1)
-      q_exp = tf.expand_dims(q, axis=-1)
-      sigma_exp = tf.expand_dims(sigma, axis=-1)
-      ratio = b(tau_exp, k_exp, r_exp, q_exp) / b(u, k_exp, r_exp, q_exp)
-      norm = _standard_normal_cdf(
-          _d_plus(tau_exp - u, ratio, r_exp, q_exp, sigma_exp))
-      return tf.math.exp(q_exp * u) * norm
-
-    term2 = q * gauss_legendre(
-        func=func,
-        lower=tf.zeros_like(tau),
-        upper=tau,
-        num_points=integration_num_points,
-        dtype=dtype)
-    return term1 + term2
+    # Shape [num_options, 1]
+    k_exp = tf.expand_dims(k, axis=-1)
+    r_exp = tf.expand_dims(r, axis=-1)
+    q_exp = tf.expand_dims(q, axis=-1)
+    # Shape [num_options, grid_num_points, 1]
+    tau_grid_exp = tf.expand_dims(tau_grid, axis=-1)
+    def b_0(tau_grid_exp):
+      # Shape [num_options, grid_num_points, 1]
+      k_exp_exp = tf.expand_dims(k_exp, axis=-1)
+      r_exp_exp = tf.expand_dims(r_exp, axis=-1)
+      q_exp_exp = tf.expand_dims(q_exp, axis=-1)
+      return tf.ones_like(tau_grid_exp) * k_exp_exp * tf.math.minimum(
+          tf.constant(1.0, dtype=dtype), r_exp_exp / q_exp_exp)
+    def cond(converged, _):
+      return tf.math.logical_not(converged)
+    def body(converged, current_boundary_points):
+      # Shape [num_options, grid_num_points]
+      spline_params = build_spline(tau_grid, current_boundary_points)
+      def current_exercise_boundary_fn(tau_grid_exp):
+        # Reshaping because interpolation needs last dimension to be > 1
+        shape_1 = utils.get_shape(tau_grid_exp)[1]
+        shape_2 = utils.get_shape(tau_grid_exp)[2]
+        tau_grid_exp_reshape = tf.reshape(tau_grid_exp, [-1, shape_1 * shape_2])
+        interpolation = interpolate(tau_grid_exp_reshape, spline_params)
+        return tf.reshape(interpolation, [-1, shape_1, shape_2])
+      # Shape [num_options, grid_num_points]
+      numerator = boundary_numerator(tau_grid, current_exercise_boundary_fn, k,
+                                     r, q, sigma, integration_num_points)
+      denominator = boundary_denominator(tau_grid, current_exercise_boundary_fn,
+                                         k, r, q, sigma, integration_num_points)
+      new_boundary_points = tf.math.divide_no_nan(
+          k_exp * tf.math.exp(-(r_exp - q_exp) * tau_grid) * numerator,
+          denominator)
+      diff = new_boundary_points - current_boundary_points
+      # Shape 0
+      relative_error = tf.math.reduce_max(
+          tf.math.abs(diff) / (tf.math.abs(new_boundary_points) + epsilon))
+      converged = relative_error <= tolerance
+      return converged, new_boundary_points
+    # Shape 0
+    converged = tf.constant(False)
+    # Shapes 0, [num_options, grid_num_points]
+    loop_vars = (converged, tf.squeeze(b_0(tau_grid_exp), axis=-1))
+    # Shapes 0, [num_options, grid_num_points]
+    _, result = tf.while_loop(
+        cond=cond, body=body, loop_vars=loop_vars, maximum_iterations=max_depth)
+    # Shape [num_options, grid_num_points]
+    new_spline_params = build_spline(tau_grid, result)
+    def new_exercise_boundary_fn(tau_grid_exp):
+      # Reshaping because interpolation needs last dimension to be > 1
+      shape_1 = utils.get_shape(tau_grid_exp)[1]
+      shape_2 = utils.get_shape(tau_grid_exp)[2]
+      tau_grid_exp_reshape = tf.reshape(tau_grid_exp, [-1, shape_1 * shape_2])
+      interpolation = interpolate(tau_grid_exp_reshape, new_spline_params)
+      return tf.reshape(interpolation, [-1, shape_1, shape_2])
+    return new_exercise_boundary_fn
--- tf_quant_finance/experimental/american_option_pricing/exercise_boundary_test.py
+++ tf_quant_finance/experimental/american_option_pricing/exercise_boundary_test.py
@@ -8,70 +8,67 @@ from tensorflow.python.framework import test_util  # pylint: disable=g-direct-te
 
 boundary_numerator = tff.experimental.american_option_pricing.exercise_boundary.boundary_numerator
 boundary_denominator = tff.experimental.american_option_pricing.exercise_boundary.boundary_denominator
+exercise_boundary = tff.experimental.american_option_pricing.exercise_boundary.exercise_boundary
 
 
+@test_util.run_all_in_graph_and_eager_modes
 class ExerciseBoundaryTest(parameterized.TestCase, tf.test.TestCase):
 
   @parameterized.named_parameters(
       {
-          'testcase_name': 'TestEmpty',
-          'k': [],
-          'tau': [],
-          'r': [],
-          'q': [],
-          'sigma': [],
-          'expected_numerator': []
-      },
-      {
-          'testcase_name': 'TestSingle',
-          'k': 45,
-          'tau': 3,
-          'r': 0.15,
-          'q': 0.05,
-          'sigma': 0.02,
-          'expected_numerator': 1.56363801,
-      },
-      {
           'testcase_name': 'TestMultiple',
           'k': [100, 100],
-          'tau': [1, 2],
+          'tau_grid': [[0., 0.5, 1.], [0., 1., 2.]],
           'r': [0.01, 0.02],
           'q': [0.01, 0.02],
           'sigma': [0.1, 0.15],
-          'expected_numerator': [0.4849528, 0.47702501],
-      },
-  )
-  def test_numerator(self, k, tau, r, q, sigma, expected_numerator):
+          'expected_shape': (2, 3)
+      },)
+  def test_numerator(self, k, tau_grid, r, q, sigma, expected_shape):
     k = tf.constant(k, dtype=tf.float64)
-    tau = tf.constant(tau, dtype=tf.float64)
+    tau_grid = tf.constant(tau_grid, dtype=tf.float64)
     r = tf.constant(r, dtype=tf.float64)
     q = tf.constant(q, dtype=tf.float64)
     sigma = tf.constant(sigma, dtype=tf.float64)
-    expected_numerator = tf.constant(expected_numerator, dtype=tf.float64)
-    def _b_0(_, k, r, q):
-      return k * tf.math.minimum(1, r/q)
-    actual_numerator = boundary_numerator(tau, _b_0, k, r, q, sigma)
-    np.testing.assert_allclose(actual_numerator, expected_numerator, rtol=1e-8)
+    # TODO(viktoriac): test against numpy results
+    k_exp = k[:, tf.newaxis, tf.newaxis]
+    r_exp = r[:, tf.newaxis, tf.newaxis]
+    q_exp = q[:, tf.newaxis, tf.newaxis]
+    def _b_0(tau_grid_exp):
+      return tf.ones_like(tau_grid_exp) * k_exp * tf.math.minimum(
+          tf.constant(1.0, dtype=tf.float64), r_exp / q_exp)
+    actual_numerator = boundary_numerator(tau_grid, _b_0, k, r, q, sigma)
+    actual_numerator = np.array(self.evaluate(actual_numerator))
+    self.assertEqual(actual_numerator.shape, expected_shape)
 
   @parameterized.named_parameters(
       {
-          'testcase_name': 'TestEmpty',
-          'k': [],
-          'tau': [],
-          'r': [],
-          'q': [],
-          'sigma': [],
-          'expected_denominator': []
-      },
-      {
-          'testcase_name': 'TestSingle',
-          'k': 45,
-          'tau': 3,
-          'r': 0.15,
-          'q': 0.05,
-          'sigma': 0.02,
-          'expected_denominator': 1.1606823164169706,
-      },
+          'testcase_name': 'TestMultiple',
+          'k': [100, 100],
+          'tau_grid': [[0., 0.5, 1.], [0., 1., 2.]],
+          'r': [0.01, 0.02],
+          'q': [0.01, 0.02],
+          'sigma': [0.1, 0.15],
+          'expected_shape': (2, 3),
+      },)
+  def test_denominator(self, k, tau_grid, r, q, sigma, expected_shape):
+    k = tf.constant(k, dtype=tf.float64)
+    tau_grid = tf.constant(tau_grid, dtype=tf.float64)
+    r = tf.constant(r, dtype=tf.float64)
+    q = tf.constant(q, dtype=tf.float64)
+    sigma = tf.constant(sigma, dtype=tf.float64)
+    # TODO(viktoriac): test against numpy results
+    k_exp = k[:, tf.newaxis, tf.newaxis]
+    r_exp = r[:, tf.newaxis, tf.newaxis]
+    q_exp = q[:, tf.newaxis, tf.newaxis]
+    def _b_0(tau_grid_exp):
+      return tf.ones_like(tau_grid_exp) * k_exp * tf.math.minimum(
+          tf.constant(1.0, dtype=tf.float64), r_exp / q_exp)
+    actual_denominator = boundary_denominator(tau_grid, _b_0, k, r, q, sigma)
+    actual_denominator = np.array(self.evaluate(actual_denominator))
+    self.assertEqual(actual_denominator.shape, expected_shape)
+
+  @parameterized.named_parameters(
       {
           'testcase_name': 'TestMultiple',
           'k': [100, 100],
@@ -79,21 +76,48 @@ class ExerciseBoundaryTest(parameterized.TestCase, tf.test.TestCase):
           'r': [0.01, 0.02],
           'q': [0.01, 0.02],
           'sigma': [0.1, 0.15],
-          'expected_denominator': [0.52509737, 0.56378576],
-      },
-  )
-  def test_denominator(self, k, tau, r, q, sigma, expected_denominator):
+          'grid_num_points': 3,
+          'max_depth': 30,
+          'tolerance': 1e-8,
+          'integration_num_points': 32,
+          'expected_shape': (2, 3),
+      },)
+  def test_exercise_boundary(self, k, tau, r, q, sigma, grid_num_points,
+                             max_depth, tolerance, integration_num_points,
+                             expected_shape):
     k = tf.constant(k, dtype=tf.float64)
     tau = tf.constant(tau, dtype=tf.float64)
+    tau_grid = tf.linspace(
+        tf.constant(0., dtype=tf.float64), tau, grid_num_points, axis=-1)
     r = tf.constant(r, dtype=tf.float64)
     q = tf.constant(q, dtype=tf.float64)
     sigma = tf.constant(sigma, dtype=tf.float64)
-    expected_denominator = tf.constant(expected_denominator, dtype=tf.float64)
-    def _b_0(_, k, r, q):
-      return k * tf.math.minimum(1, r/q)
-    actual_denominator = boundary_denominator(tau, _b_0, k, r, q, sigma)
-    np.testing.assert_allclose(
-        actual_denominator, expected_denominator, rtol=1e-8)
+    k_exp = tf.expand_dims(k, axis=-1)
+    r_exp = tf.expand_dims(r, axis=-1)
+    q_exp = tf.expand_dims(q, axis=-1)
+    tau_grid_exp = tf.expand_dims(tau_grid, axis=-1)
+    # TODO(viktoriac): test against numpy results
+    actual_boundary_function = exercise_boundary(tau_grid, k, r, q, sigma,
+                                                 max_depth,
+                                                 tolerance,
+                                                 integration_num_points,
+                                                 dtype=tf.float64)
+    actual_boundary_exp = actual_boundary_function(tau_grid_exp)
+    actual_boundary = tf.squeeze(actual_boundary_exp)
+    # Test if next iteration of the function gives same results
+    numerator = boundary_numerator(tau_grid, actual_boundary_function, k, r, q,
+                                   sigma, integration_num_points)
+    denominator = boundary_denominator(tau_grid, actual_boundary_function, k, r,
+                                       q, sigma, integration_num_points)
+    next_boundary_points = tf.math.divide_no_nan(
+        k_exp * tf.math.exp(-(r_exp - q_exp) * tau_grid) * numerator,
+        denominator)
+    actual_boundary = np.array(self.evaluate(actual_boundary))
+    next_boundary_points = np.array(self.evaluate(next_boundary_points))
+    # Test results against next iteration of exercise boundary function
+    np.testing.assert_allclose(actual_boundary, next_boundary_points, rtol=1e-8)
+    # Test shape of result
+    self.assertEqual(actual_boundary.shape, expected_shape)
 
 
 if __name__ == '__main__':
